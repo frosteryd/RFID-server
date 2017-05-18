687c2b3bf2dead4f8d27af1e098b5613ebaee889
Linus Frosteryd <linus.frosteryd@gmail.com>
2017-05-18 12:20:50 +0200

    LINUS OCH TJABO FINAL DONE!

--- sketchbook/EMCU/EMCU.ino
+++ sketchbook/EMCU/EMCU.ino
@@ -1,10 +1,10 @@
 /*
- * 
- * All the resources for this project: https://www.hackster.io/Aritro
- * Modified by Aritro Mukherjee
- * 
- * 
- */
+
+   All the resources for this project: https://www.hackster.io/Aritro
+   Modified by Aritro Mukherjee
+
+
+*/
 
 #include <SPI.h>
 #include <MFRC522.h>
@@ -13,7 +13,7 @@
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
 char RFID_data[12], data_store[10][12];
-int array_count = 1; 
+int array_count = 1;
 
 int busy = 0;
 int mode = 0;
@@ -28,7 +28,7 @@ int mode_button = 6;
 int seg_bit_0 = 7;
 int seg_bit_1 = 8;
 
-void setup() 
+void setup()
 {
   pinMode(green_led, OUTPUT);
   pinMode(red_led, OUTPUT);
@@ -44,14 +44,15 @@ void setup()
   mfrc522.PCD_Init();   // Initiate MFRC522
   Serial.println("Approximate your card to the reader...");
   Serial.println();
-  strcpy(data_store[1], "EE A3 41 D5");
+  String startword = "EE A3 41 D5";
+  startword.toCharArray(data_store[1], 12);
 
 }
 void loop()
 {
-  if(busy == 0) { // Might be used to prevent mulitple things running at the same time
+  if (busy == 0) { // Might be used to prevent mulitple things running at the same time
     /* Check if the button is pressed and change mode */
-    if(digitalRead(mode_button) == LOW) {
+    if (digitalRead(mode_button) == LOW) {
       setBusy(1);
       mode++;
       delay(100);
@@ -61,138 +62,143 @@ void loop()
     }
 
     /* Perform action based on what mode is set */
-    if(mode == 0) { // Mode 0 is default and i looking for a card to be swiped
+    if (mode == 0) { // Mode 0 is default and i looking for a card to be swiped
       segDisplay(0);
       CheckRFID();
       access = 0;
-    } 
-    else if(mode == 1) { // Mode 1 is for adding new cards to the list of auth users
+    }
+    else if (mode == 1) { // Mode 1 is for adding new cards to the list of auth users
       segDisplay(1);
       AddCard();
       access = 0;
-    } 
-    else if(mode == 2) { // Mode 2 is for removing access for cards
+    }
+    else if (mode == 2) { // Mode 2 is for removing access for cards
       segDisplay(2);
+      RemoveCard();
       access = 0;
-    } 
+    }
     else { // If the mode doesn't match any exsiting it resets to 0
       mode = 0;
     }
   }
 }
 
-void AddCard(){
+void AddCard() {
   int cardAuthorized = CheckRFID();
-  delay(1000);
-  if(cardAuthorized == 1){
-     while(true){
-              // Look for new cards
-        if (mfrc522.PICC_IsNewCardPresent()) 
-        {
-          Serial.println("Retunerar med första");
-          break;
-        }
-        // Select one of the cards
-        if (mfrc522.PICC_ReadCardSerial()) 
-        {
-          Serial.println("Retunerar med andra");
-          break;
-        }
-    }
-        Serial.println("Registerar kort och låser : ");
-        setBusy(1);
-        //Show UID on serial monitor
-        Serial.print("UID tag :");
-        String content= "";
-        byte letter;
-        for (byte i = 0; i < mfrc522.uid.size; i++) 
-        {
-          Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-          Serial.print(mfrc522.uid.uidByte[i], HEX);
-          content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-          content.concat(String(mfrc522.uid.uidByte[i], HEX));
-        }
-        Serial.println();
-        Serial.print("Message : ");
-        content.toUpperCase();
-        String Compareword = content.substring(1); // HÄR ÄR KODEN PÅ KORTET SOM SKA REGISTRERAS
+  delay(2000);
+  if (cardAuthorized == 1) {
+    //    while(true){
 
-        array_count++;
-        strcpy(data_store[array_count], "AF B5 1E 56"); // HÄR ÄR DÄR DEN STRÄNGEN SKA TRYCKAS IN! 
-        Serial.println("Ska vara added : ");
-      setBusy(0);
-  } else {
-    
-  }  
+    Serial.println("Väntar på nytt : ");
+    delay(2000);
+    if (!mfrc522.PICC_IsNewCardPresent())  //TODO LÄGG IN I LOOP FÖR ATT VÄNTA PÅ ETT KORT
+    {
+      return;
+    }
+    // Select one of the cards
+    if (!mfrc522.PICC_ReadCardSerial())
+    {
+      return;
+    }
+    setBusy(1);
+
+    String content;
+    for (byte i = 0; i < mfrc522.uid.size; i++)
+    {
+      Serial.print("CARD ADDED: ");
+      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+      Serial.print(mfrc522.uid.uidByte[i], HEX);
+      content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+      content.concat(String(mfrc522.uid.uidByte[i], HEX));
+    }
+
+    content.toUpperCase();
+    array_count++;
+    String Cword = content.substring(1); //"AF B5 1E 56";
+    Cword.toCharArray(data_store[array_count], 12);
+    Serial.println("Går ur loop : ");
+    setBusy(0);
+  }  else {
+
+  }
 
 }
 
-void RemoveCard(){
+void RemoveCard() {
   int cardAuthorized = CheckRFID();
   delay(1000);
-  if(cardAuthorized == 1){
-     while(true){
-              // Look for new cards
-        if (mfrc522.PICC_IsNewCardPresent()) 
-        {
-          Serial.println("Retunerar med första");
-          break;
-        }
-        // Select one of the cards
-        if (mfrc522.PICC_ReadCardSerial()) 
-        {
-          Serial.println("Retunerar med andra");
-          break;
-        }
-    }
-        Serial.println("Registerar kort och låser : ");
-        setBusy(1);
-        //Show UID on serial monitor
-        Serial.print("UID tag :");
-        String content= "";
-        byte letter;
-        for (byte i = 0; i < mfrc522.uid.size; i++) 
-        {
-          Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-          Serial.print(mfrc522.uid.uidByte[i], HEX);
-          content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-          content.concat(String(mfrc522.uid.uidByte[i], HEX));
-        }
-        Serial.println();
-        Serial.print("Message : ");
-        content.toUpperCase();
-        String Compareword = content.substring(1); // HÄR ÄR KODEN PÅ KORTET SOM SKA TAS BORT
+  if (cardAuthorized == 1) {
 
-        array_count--;
-        // TODO SÖKA I ARRAY EFTER KODEN OCH TA BORT DEN. AUTO INCREMENTA LISTAN 
-        Serial.println("Ska vara Borttagen : ");
-      setBusy(0);
+    Serial.println("Väntar på nytt : ");
+    delay(2000);
+    if (!mfrc522.PICC_IsNewCardPresent())  //TODO LÄGG IN I LOOP FÖR ATT VÄNTA PÅ ETT KORT
+    {
+      return;
+    }
+    // Select one of the cards
+    if (!mfrc522.PICC_ReadCardSerial())
+    {
+      return;
+    }
+    setBusy(1);
+
+    String content;
+    for (byte i = 0; i < mfrc522.uid.size; i++)
+    {
+      Serial.print("CARD DELETED: ");
+      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+      Serial.print(mfrc522.uid.uidByte[i], HEX);
+      content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+      content.concat(String(mfrc522.uid.uidByte[i], HEX));
+    }
+    // SÖK EFTER STRING I CHAR ARRAY
+    // TA BORT STRINGEN PÅ DET INDEX
+    // SKRIV OM LISTAN MED NYY INDEX
+    // ARRAY COUNT --
+    content.toUpperCase();
+    array_count++;
+    char tempDeletString[12];
+    String Cword = content.substring(1); //"AF B5 1E 56";
+    Cword.toCharArray(tempDeletString, 12);
+    char *s;
+    for( int i=0; i <10; i++){
+      s = strstr(data_store[i],tempDeletString);
+      if(s != NULL){
+        memset(&data_store[i], 0, 12);
+        Serial.println("Kort borttaget");
+        
+      }
+      else{
+      }   
+    }
+    Serial.println("Går ur loop : ");
+    setBusy(0);
   } else {
-    
-  }  
+
+  }
 
 }
 
 /*
   Function to scan for keycards and verify them.
 */
-int CheckRFID(){
-    // Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+int CheckRFID() {
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent())
   {
     return 0;
   }
   // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  if ( ! mfrc522.PICC_ReadCardSerial())
   {
     return 0;
   }
   setBusy(1);
   //Show UID on serial monitor
   Serial.print("UID tag :");
-  String content= "";
+  String content = "";
   byte letter;
-  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  for (byte i = 0; i < mfrc522.uid.size; i++)
   {
     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
     Serial.print(mfrc522.uid.uidByte[i], HEX);
@@ -204,39 +210,39 @@ int CheckRFID(){
   content.toUpperCase();
   String Compareword = content.substring(1);
   int checkCardAccess = CheckCard(Compareword);
-      if (checkCardAccess == 1) 
-      {
-        Serial.println("Authorized access");
-        Serial.println();
-        ledBlink(green_led, 1000);
-        delay(500);
-        setBusy(0);
-        return 1;
-      }
-    
-      else   {
-        Serial.println("Not authorized access");
-        Serial.println();
-        ledBlink(red_led, 1000);
-        delay(500);
-        setBusy(0);
-        return 0;
-      }
+  if (checkCardAccess == 1)
+  {
+    Serial.println("Authorized access");
+    Serial.println();
+    ledBlink(green_led, 1000);
+    delay(500);
+    setBusy(0);
+    return 1;
+  }
+
+  else   {
+    Serial.println("Not authorized access");
+    Serial.println();
+    ledBlink(red_led, 1000);
+    delay(500);
+    setBusy(0);
+    return 0;
+  }
 }
 
-int CheckCard(String Password){
+int CheckCard(String Password) {
 
-  for( int i = 0; i < 10; i++)
-    if (Password == data_store[i]) 
-      {
+  for ( int i = 0; i < 10; i++)
+    if (Password == data_store[i])
+    {
       return 1;
 
-      }
-    
-      else   {
+    }
 
-      }
-    return 0;
+    else   {
+
+    }
+  return 0;
 }
 
 void ledOn(int led_nr) {
@@ -254,14 +260,14 @@ void ledBlink(int led_nr, int delay_time) {
 }
 
 void segDisplay(int nr) {
-  if(nr == 1) {
+  if (nr == 1) {
     ledOn(seg_bit_0);
     ledOff(seg_bit_1);
-  } 
-  else if(nr == 2) {
+  }
+  else if (nr == 2) {
     ledOff(seg_bit_0);
     ledOn(seg_bit_1);
-  } 
+  }
   else {
     ledOff(seg_bit_0);
     ledOff(seg_bit_1);
@@ -269,13 +275,12 @@ void segDisplay(int nr) {
 }
 
 void setBusy(int busy) {
-  if(busy == 1) {
+  if (busy == 1) {
     busy = 1;
     ledOn(blue_led);
-  } 
+  }
   else {
     busy = 0;
     ledOff(blue_led);
   }
 }
-
